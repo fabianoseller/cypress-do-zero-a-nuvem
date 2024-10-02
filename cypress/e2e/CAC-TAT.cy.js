40a94da0163e7d1c4eddbce1f50bb29269fa7541
Fabiano Antunes <fabianoseller@gmail.com>
2024-10-02 20:53:54 -0300

    /walmyr aula 01 rconfig1 passed

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,5 +1,7 @@
 describe('Central de atendimento ao Cliente TAT', () => {
     it('Verifica o título da aplicação', () => {
-        cy.visit('.src/index.html')
+        cy.visit('./src/index.html')
+
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
 })
